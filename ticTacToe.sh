e5f53c6da43a4af3d432530f4dc44e4880a4a095
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-27 10:53:51 +0530

    MODIFIED: Added feature to display fresh board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
-printf "Welcome and play Tic Tac Toe game\n"
-
+printf "Display fresh board\n"
+count=0
+declare -a displayBoard
+displayBoard=(" " " " " " " " " " " " " " " " " ")
+for(( index=0; index<${#displayBoard[@]}; index++))
+do
+	printf " ${displayBoard[$index]} | "
+	count=$((count+1))
+	if(($count%3==0 && count!=9))
+	then
+		printf "\n---+----+----+----\n"
+	fi
+done
+printf "\n"
