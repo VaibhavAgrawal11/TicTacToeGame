a495043e4c54d7125fc4ca5ddc3d9dc3d163bf44
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-28 11:05:24 +0530

    MODIFIED: Added feature to check win / draw or change.

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-printf "Added feature for player turn.\n"
+printf "Added feature to check win / draw or change.\n"
 #CONSTANTS
 PLAYER=0
 COMPUTER=1
@@ -29,6 +29,36 @@ function displayBoard(){
 	printf "\n"
 }
 
+#LOGIC TO ASSIGN LETTER
+function assignLetter()
+{
+	random=$((RANDOM%2))
+	case $random in
+	   $DOT)
+	   playerLetter=O
+		computerLetter=X
+	   ;;
+	   $CROSS)
+	   playerLetter=X
+		computerLetter=O
+	   ;;
+	esac
+}
+
+#LOGIC TO GIVE FIRST CHANCE RANDOMLY
+function firstChance()
+{
+	checkChance=$((RANDOM%2))
+	case $checkChance in
+   	$PLAYER)
+   	printf "playerChance\n"
+   	;;
+   	$COMPUTER)
+   	printf "computerChance\n"
+   	;;
+	esac
+}
+
 #LOGIC FOR PLAYER TURN
 function playerTurn(){
 	playerLetter=$1
@@ -44,23 +74,58 @@ function playerTurn(){
 	displayBoard
 }
 
+#LOGIC TO CHECK THE WINNER ON EVERY MOVE
+function checkWin(){
+	letter=$1
+	if [ "${display[0]}${display[1]}${display[2]}" = "$letter$letter$letter" ];
+ 	then
+		result="wins"
+	elif [ "${display[3]}${display[4]}${display[5]}" = "$letter$letter$letter" ]
+   then
+      result="wins"
+	elif [ "${display[6]}${display[7]}${display[8]}" = "$letter$letter$letter" ]
+   then
+      result="wins"
+	elif [ "${display[0]}${display[3]}${display[6]}" = "$letter$letter$letter" ]
+   then
+      result="wins"
+	elif [ "${display[1]}${display[4]}${display[7]}" = "$letter$letter$letter" ]
+   then
+      result="wins"
+	elif [ "${display[2]}${display[5]}${display[8]}" = "$letter$letter$letter" ]
+   then
+      result="wins"
+	elif [ "${display[0]}${display[3]}${display[6]}" = "$letter$letter$letter" ]
+   then
+      result="wins"
+	elif [ "${display[0]}${display[4]}${display[8]}" = "$letter$letter$letter" ]
+   then
+      result="wins"
+	elif [ "${display[2]}${display[4]}${display[6]}" = "$letter$letter$letter" ]
+   then
+      result="wins"
+	else
+		flag=0
+		for(( index=0; index<${#display[@]}; index++))
+		do
+			if [[ "${display[$index]}"!=X || "${display[$index]}"!=O ]]
+			then
+				flag=1
+			fi
+		done
+		if(($flag==0))
+		then
+			result="draw"
+		else
+			result="change"
+		fi
+fi
+echo $result
+}
+
 displayBoard
-random=$((RANDOM%2))
-case $random in
-	$DOT)
-	playerLetter=O
-	;;
-	$CROSS)
-	playerLetter=X
-	;;
-esac
-checkChance=$((RANDOM%2))
-case $checkChance in
-	$PLAYER)
-	printf "playerChance\n"
-	;;
-	$COMPUTER)
-	printf "computerChance\n"
-	;;
-esac
+assignLetter
+chance="$(firstChance)"
+echo "$chance"
 playerTurn $playerLetter
+checkWin $playerLetter
